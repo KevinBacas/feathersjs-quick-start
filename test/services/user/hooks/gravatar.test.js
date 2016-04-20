0b41d5382605515e54ccc2027362af01a1fd1505
Kevin Bacas <bacas.kevin@hotmail.fr>
2016-04-21 00:23:47 +0200

    Fix Windows paths + tests

--- test/services/user/hooks/gravatar.test.js
+++ test/services/user/hooks/gravatar.test.js
@@ -1,20 +1,27 @@
 'use strict';
 
 const assert = require('assert');
-const gravatar = require('../../../../src\services\user\hooks\gravatar.js');
+const gravatar = require('../../../../src/services/user/hooks/gravatar.js');
 
-describe('user gravatar hook', function() {
-  it('hook can be used', function() {
+describe('user gravatar hook', () => {
+  it('returns a function', () => {
+    var hook = gravatar();
+    assert.equal(typeof hook, 'function');
+  });
+
+  it('returns a gravatar url', () => {
     const mockHook = {
       type: 'before',
       app: {},
       params: {},
       result: {},
-      data: {}
+      data: {
+        email: 'hello@feathersjs.com'
+      }
     };
-
+    
     gravatar()(mockHook);
-
-    assert.ok(mockHook.gravatar);
+    
+    assert.ok(mockHook.data.avatar);
   });
 });
